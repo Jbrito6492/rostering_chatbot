c9b14256d564c841b0c76cf811a90eb1302b653f
Josh Brito <josh.brito@strongmind.com>
2024-06-23 16:59:22 -0700

    Add Message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_231443) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_23_235828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,4 +19,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_23_231443) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "conversation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+  end
+
+  add_foreign_key "messages", "conversations"
 end
