0f261ca4be383b1d9bc427354bde26644d7a83a8
Josh Brito <josh.brito@strongmind.com>
2024-06-23 17:55:13 -0700

    Changes root route to be conversations#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :messages
   resources :conversations
-  root to: "messages#new"
+  root to: "conversations#new"
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
