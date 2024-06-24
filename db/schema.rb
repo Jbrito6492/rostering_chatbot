5441cc36bf2f287216e93b9f409d6ed7fb9e94ca
Josh Brito <josh.brito@strongmind.com>
2024-06-23 17:59:53 -0700

    Add role to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_24_004626) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_24_005819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_24_004626) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "llm_provider"
+    t.string "role"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
