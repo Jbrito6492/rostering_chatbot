613ac8e9b89071727b006ec96c007913ec7588d0
Josh Brito <josh.brito@strongmind.com>
2024-06-23 18:02:45 -0700

    Add generate response to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :conversation
+
+  after_create :generate_response, if: -> { role == 'user' }
+
+  def generate_response
+    client = conversation.one_roster_integrated_client(llm: llm_provider)
+    response = client.ask(question: content)
+    conversation.messages.create!(content: response, role: 'bot', llm_provider: llm_provider)
+  end
 end
