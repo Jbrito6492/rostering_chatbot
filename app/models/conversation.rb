e5f0c51c18509fad68fb493538d2dbf740784fb6
Josh Brito <josh.brito@strongmind.com>
2024-06-23 17:26:16 -0700

    Raise if invalid llm provided

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,9 +4,11 @@ class Conversation < ApplicationRecord
   def client(llm: 'bedrock')
     if llm == 'bedrock'
       Langchain::LLM::AwsBedrock.new(completion_model: 'anthropic.claude-3-sonnet-20240229-v1:0')
-    else
+    elsif llm == 'openai'
       Langchain::LLM::OpenAI.new(api_key: Rails.application.credentials.open_ai[:api_key],
                                  default_options: { chat_completion_model_name: 'gpt-4' })
+    else
+      raise ArgumentError.new("Invalid LLM type: #{llm}")
     end
   end
 end
