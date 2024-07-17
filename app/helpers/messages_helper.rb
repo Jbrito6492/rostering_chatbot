c08cf63ad9307385f0856a7e3caee8f72d75708f
Josh Brito <josh.brito@strongmind.com>
2024-07-16 23:34:23 -0700

    (S)  - Add JSON formatting and syntax highlighting

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,14 @@
 module MessagesHelper
+  def format_content_with_embedded_json(content)
+    content.gsub!(/```(.*?)```/m) do
+      json_string = $1.strip
+      begin
+        json_pretty = JSON.pretty_generate(JSON.parse(json_string))
+        "<pre class='bg-gray-200 p-2 rounded'><code>#{json_pretty}</code></pre>"
+      rescue JSON::ParserError, JSON::GeneratorError
+        "<pre class='bg-gray-200 p-2 rounded'><code>#{json_string}</code></pre>"
+      end
+    end
+    content.html_safe
+  end
 end
